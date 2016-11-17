510bb4e3b333f74ef7fcb4449c909d38c5711ba6
StuartD <stuartd96@live.co.uk>
2016-11-17 11:22:46 +0000

    Version 0.5
    Added gitignore, Added more shortlink sites, removed dodgy apple.co
    link.

--- extender.js
+++ extender.js
@@ -1,18 +1,34 @@
+/**
+ * Shortlink Extender
+ * Developer: StuartD - www.stuartd.co.uk
+ */
+
 var shortlinks = [
-    "bit.ly", 
-    "goo.gl", 
-    "apple.co", 
-    "tinyurl.com"
+    "bit.ly",
+    "goo.gl",
+    "tinyurl.com",
+    "7.ly",
+    "adf.ly",
+    "bit.do",
+    "fur.ly",
+    "is.gd",
+    "tiny.cc",
+    "tny.im",
+    "v.gd",
+    "y2u.be",
+    "git.io"
 ];
 
 var links = document.querySelectorAll("a");
+
 for(var i = 0; i < links.length; i++){
-    var URL = links[i].href;
-    
     for(var j = 0; j < shortlinks.length; j++){
         if(links[i].href.indexOf(shortlinks[j]) !== -1){
-            var data = httpGet("//host.stuartd.co.uk/api.php?url=" + links[i].href);
-            links[i].innerHTML = links[i].text + " <span style=\"font-size: 0.7em\">(" + data + ")</span>";
+            var endID = links[i].href.split(shortlinks[j].slice(-1))[1].substring(1);
+            if(endID !== ""){
+                var data = httpGet("//host.stuartd.co.uk/api.php?url=" + links[i].href);
+                links[i].innerHTML = links[i].text + '<span style="margin-left: 0.5em;display: inline-block;border-radius: 2px;padding: 0.2em 0.5em;background-color: #1099ad;color: white;font-size: 0.8em;">' + data + '</span>';
+            }
         }
     }
 }
